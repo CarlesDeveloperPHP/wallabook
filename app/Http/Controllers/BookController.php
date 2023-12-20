1aad98ae76f6e555217e7bc21206f7c1230e90d5
CarlesDeveloperPHP <crmfeina@gmail.com>
2023-12-20 09:43:04 +0100

    Login finalizado en castellano y testeado.

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index(){
+        return view('books.index');
+    }
+
+    public function create(){
+        return view('books.create');
+    }
+
+    public function show($libro){
+        
+        //return view('books.show', ['libro' => $libro]); Es lo mismo que compact pero han de coincidir el nombre de la variable
+        return view('books.show', compact('libro'));
+    }
+}
---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantillaForms')
+
+@extends('title', 'wellcome')
+
+@section('content')
+    <h1>creamos libro</h1>
+@endsection
+
