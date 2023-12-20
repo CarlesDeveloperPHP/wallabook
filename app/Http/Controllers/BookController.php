2b8e6558fc7751ff3f8a719b1c15905e469c277f
CarlesDeveloperPHP <crmfeina@gmail.com>
2023-12-20 12:30:45 +0100

    Finalizado login

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index(){
-        return view('books.index');
-    }
-
     public function create(){
         return view('books.create');
     }
